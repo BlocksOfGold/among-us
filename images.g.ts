fe84445870c5816268eb5e4430f40658664bc05c
BlocksOfGold <117589512+BlocksOfGold@users.noreply.github.com>
2022-11-21 16:41:06 -0600

    admin and navigation added

--- images.g.ts
+++ images.g.ts
@@ -22,11 +22,56 @@ namespace myImages {
 . . . . f f f . . . f f . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 4 4 4 4 4 4 4 f . . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . f f f f f f 4 4 4 4 f f . . 
+. f 9 9 9 9 9 f 4 4 4 4 f 4 f . 
+. f 9 9 9 9 9 f 4 4 4 4 f 4 f . 
+. . f f f f f f 4 4 4 4 f 4 f . 
+. . . f 4 4 4 4 4 4 4 4 f 4 f . 
+. . . f 4 4 4 4 4 4 4 4 f 4 f . 
+. . . f 4 4 4 4 4 4 4 4 f f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . . f 4 4 4 f f f 4 4 f . . . 
+. . . . f f f . . . f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . b b b b b b b . . . . 
+. . . . b f f f f f f f b . . . 
+. . . b f f f f f f f f b . . . 
+. . b b b b b b f f f f b b . . 
+. b 9 9 9 9 9 b f f f f b f b . 
+. b 9 9 9 9 9 b f f f f b f b . 
+. . b b b b b b f f f f b f b . 
+. . . b f f f f f f f f b f b . 
+. . . b f f f f f f f f b f b . 
+. . . b f f f f f f f f b b . . 
+. . . b f f f f f f f f b . . . 
+. . . b f f f b b b f f b . . . 
+. . . . b b b . . . b b . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
         switch(helpers.stringTrim(name)) {
 
         }
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const crew = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile19`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level26`)
 })
@@ -10,8 +13,13 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile22`, function (sprite, 
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level7`)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level36`)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile25`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level7`)
+    tiles.placeOnRandomTile(imposter, assets.tile`myTile0`)
+    OrangeCrew.destroy()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level7`)
@@ -35,13 +43,43 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile17`, function (sprite, 
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level1`)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.crew, function (sprite, otherSprite) {
+    if (otherSprite == OrangeCrew) {
+        if (controller.A.isPressed()) {
+            Orange += 1
+            OrangeCrew.destroy(effects.fire, 500)
+        }
+    }
+    if (otherSprite == BlackCrew) {
+        if (controller.A.isPressed()) {
+            Black += 1
+            BlackCrew.destroy(effects.fire, 500)
+        }
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level0`)
     tiles.placeOnRandomTile(imposter, assets.tile`myTile0`)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile27`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level16`)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile23`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level29`)
+    if (!(Orange >= 1)) {
+        OrangeCrew = sprites.create(assets.image`myImage0`, SpriteKind.crew)
+    }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile26`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level34`)
+    if (!(Black >= 1)) {
+        BlackCrew = sprites.create(assets.image`myImage1`, SpriteKind.crew)
+    }
+})
+let Black = 0
+let BlackCrew: Sprite = null
+let Orange = 0
+let OrangeCrew: Sprite = null
 let imposter: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 imposter = sprites.create(assets.image`myImage`, SpriteKind.Player)
